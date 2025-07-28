46842ebcb254d54717ddecf5c830ebb28b9f631c
rfullivarri <113726007+rfullivarri@users.noreply.github.com>
2025-07-28 13:32:33 +0200

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,27 @@
+let data = null;
+
+window.onload = async () => {
+    const params = new URLSearchParams(window.location.search);
+    const email = params.get("email");
+
+    if (!email) {
+        alert("❌ Falta el email");
+        return;
+    }
+
+    const res = await fetch(`https://script.google.com/macros/s/AKfycbzje0wco71mNea1v2WClcpQkvz0Ep3ZIJ8guBONQLvI3G3AXxfpdH0ECaCNMbHHcyJ3Gw/exec?email=${email}`);
+    data = await res.json();
+
+    document.getElementById("avatar").src = data.avatar_url;
+    document.getElementById("nivel").innerText = data.nivel;
+    document.getElementById("xp").innerText = data.xp;
+    document.getElementById("xp-objetivo").innerText = data.exp_objetivo;
+    document.getElementById("dias").innerText = data.dias_journey;
+
+    const xpPorcentaje = Math.round((data.xp / data.exp_objetivo) * 100);
+    document.getElementById("xp-bar").style.width = `${xpPorcentaje}%`;
+};
+
+function ir(url) {
+    if (url) window.open(url, "_blank");
+}
