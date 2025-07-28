78f51f99e20e1594d6e0b22c41d13c4025e5a587
rfullivarri <113726007+rfullivarri@users.noreply.github.com>
2025-07-28 14:44:35 +0200

    Update dashboard.js

--- js/dashboard.js
+++ js/dashboard.js
@@ -12,7 +12,7 @@ window.onload = async () => {
     const res = await fetch(`https://script.google.com/macros/s/AKfycbzje0wco71mNea1v2WClcpQkvz0Ep3ZIJ8guBONQLvI3G3AXxfpdH0ECaCNMbHHcyJ3Gw/exec?email=${email}`);
     data = await res.json();
 
-    document.getElementById("avatar").src = data.avatar_url;
+    document.getElementById("avatar").src = convertirDriveLink(data.avatar_url);
     document.getElementById("nivel").innerText = data.nivel;
     document.getElementById("xp").innerText = data.xp;
     document.getElementById("xp-objetivo").innerText = data.exp_objetivo;
@@ -21,6 +21,12 @@ window.onload = async () => {
     const xpPorcentaje = Math.round((data.xp / data.exp_objetivo) * 100);
     document.getElementById("xp-bar").style.width = `${xpPorcentaje}%`;
 };
+
+function convertirDriveLink(link) {
+    if (!link.includes("drive.google.com/file/d/")) return link;
+    const match = link.match(/\/d\/(.*?)\//);
+    return match ? `https://drive.google.com/uc?export=view&id=${match[1]}` : link;
+}
 
 function ir(url) {
     if (url) window.open(url, "_blank");
